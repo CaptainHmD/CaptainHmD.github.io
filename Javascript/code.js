7fab8d675035bd3ad73e2ce0d0cd7f5a2b9e4332
HaMaD <hamad.laaboun@gmail.com>
2022-06-23 00:40:09 +0300

    start learing Javascript

---
+++ Javascript/code.js
@@ -0,0 +1,21 @@
+console.log('hello');
+// alert('hi');
+var b = 'smoothie';
+console.log(b);
+
+
+
+document.getElementById("title1").innerHTML = name;
+function fun(){
+    var num =0
+    while(num <10){
+        console.log(num);
+        num++
+    }
+    
+    
+}
+
+let arr = new Array(12, 121 , 313);
+alert(arr[2]);
+fun();
