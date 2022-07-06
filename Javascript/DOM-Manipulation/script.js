bfce0147be57aa085abb4d5adee304b10f13ec9e
HaMaD <hamad.laaboun@gmail.com>
2022-07-06 21:47:44 +0300

    more on DOM

--- Javascript/DOM-Manipulation/script.js
+++ Javascript/DOM-Manipulation/script.js
@@ -17,5 +17,62 @@ title.innerHTML = `<h2> ${massage} .<h2>`
 
 const allSquares = document.querySelectorAll('.box-style')
 console.log(allSquares); // it will print all nodes for .box-style class
+     var timesClicked = {red: 0,'blue': 0,green: 0}
 
-     allSquares.forEach(square => square.onclick = ()=>console.log(square.value))
+
+
+     const redBox = document.getElementById('red-box')
+     const redBoxText = document.createElement('h2')
+     redBoxText.setAttribute('id','redBoxText');
+     document.getElementById('red-box').appendChild(redBoxText);
+     document.getElementById('redBoxText').innerHTML=0;
+
+
+
+     const greenBox = document.getElementById('green-box')
+     const greenBoxText = document.createElement('h2')
+     greenBoxText.setAttribute('id','greenBoxText');
+     document.getElementById('green-box').appendChild(greenBoxText);
+     document.getElementById('greenBoxText').innerHTML=0;
+
+
+     const blueBox = document.getElementById('blue-box')
+     const blueBoxText = document.createElement('h2')
+     blueBoxText.setAttribute('id','blueBoxText');
+     document.getElementById('blue-box').appendChild(blueBoxText);
+     document.getElementById('blueBoxText').innerHTML=0;
+
+
+
+     function reset(){
+          timesClicked = {red: 0,'blue': 0,green: 0}
+          document.getElementById('blueBoxText').innerHTML=0;
+          document.getElementById('greenBoxText').innerHTML=0;
+          document.getElementById('redBoxText').innerHTML=0;
+          
+     }
+
+     allSquares.forEach(square => square.onclick = ()=>{
+          console.log(square.value)
+          timesClicked[square.value]++
+          console.log(  timesClicked[square.value])
+          switch(square.value){
+               case 'red': console.log('red');
+                    document.getElementById('redBoxText').innerHTML=timesClicked.red;
+
+                    break
+               case 'green': console.log('green');
+               document.getElementById('greenBoxText').innerHTML=timesClicked.green;
+
+               break
+               case 'blue': console.log('blue');
+               document.getElementById(`blueBoxText`).innerHTML=timesClicked.blue;
+
+               break
+          }
+          
+     })
+
+
+
+
