5314d1ed5bc956c1a6fb2414055d2ead81ea8742
HaMaD <hamad.laaboun@gmail.com>
2022-07-15 07:51:01 +0300

    complete weather application

--- Javascript/Weather-app/script.js
+++ Javascript/Weather-app/script.js
@@ -1,1 +1,72 @@
-// https://rapidapi.com/community/api/open-weather-map/
+// https://rapidapi.com/community/api/open-weather-map/ 
+const options = {
+	headers: {
+		'X-RapidAPI-Key': '415c4f4c79mshbacbc03ac86e76cp1e9e49jsnc6c7054a3dce',
+	}
+};
+
+
+const apiRecquest = async (countryInput) =>{
+    const url = `https://community-open-weather-map.p.rapidapi.com/weather?q=${countryInput}&units=imperial`
+    try{
+        const response = await fetch(url,options)
+         var data = await response.json()
+         data.weather[0].main //it will do nothing just testing the error  , to go to the catch function 
+    }catch(error){
+        countryName('Invalid city');
+        return
+    }
+    countryName(data.name)
+     weatherType(data.weather[0].main)
+     Temp(data.main.temp)
+     minTemp(data.main.temp_min)
+     maxTemp(data.main.temp_max)
+
+
+}
+// apiRecquest(); // if i remove ; symbols the next function will give error why??
+
+
+const countryName = (data) =>{
+    const countryNameid = document.getElementById('country-name')
+    countryNameid.innerText=data
+
+}
+
+
+const weatherType=(data) => {
+    const countryWeatherType= document.getElementById('country-weather-type')
+    countryWeatherType.innerText=data
+}
+
+const tempClass = document.querySelectorAll('.temp') 
+const Temp=(data) => {
+    // console.log(data);
+    // console.log(tempClass[0]);
+    tempClass[0].innerText=`Temp: ${data}°`;
+}
+
+
+const minTemp=(data) => {
+    // console.log(data);
+    // console.log(tempClass[1]);
+    tempClass[1].innerText=`Min Temp: ${data}°`;
+}
+
+
+const maxTemp=(data) => {
+    // console.log(data);
+    // console.log(tempClass[2]);
+    tempClass[2].innerText=`Max Temp: ${data}°`;
+}
+
+const search= ()=>{
+    const inputSearch = document.getElementById('input')
+    const countryInput = inputSearch.value
+    apiRecquest(countryInput);
+    inputSearch.value=''
+
+
+}
+
+
